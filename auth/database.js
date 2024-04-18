d6a23897e53c3338453fe536df71ee4cb0e13b29
sanjeevashwin <sanjeevashwin045@gmail.com>
2024-04-18 12:28:34 +0530

    verified

--- auth/database.js
+++ auth/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+const sequelize = new Sequelize("0.0.0.0:5432", {
   dialect: 'postgres',
   logging: false,
 });
